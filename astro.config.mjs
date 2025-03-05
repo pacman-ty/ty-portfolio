37a0eec0de591cc808800ec2b10424a4b0596755
ty <talha.yildirim720@gmail.com>
2025-03-05 17:22:51 -0500

    deploying to netlify instead fuck this

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,8 +5,6 @@ import react from '@astrojs/react';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://pacman-ty.github.io',
-  base: '/ty-portfolio/',
   integrations: [tailwind(), react()],
   vite: {
     resolve: {
